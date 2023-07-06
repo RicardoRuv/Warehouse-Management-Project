30f6861060733d0cd2ffd0515d8608ddee76106c
RicardoRuv <75161937+RicardoRuv@users.noreply.github.com>
2023-07-06 17:22:16 -0600

    Created Item Repository interface
    Implemented findAll Items on Item service and created item repository

--- project1/src/main/java/com/skillstorm/project1/services/ItemService.java
+++ project1/src/main/java/com/skillstorm/project1/services/ItemService.java
@@ -2,14 +2,23 @@ package com.skillstorm.project1.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skillstorm.project1.models.Item;
+import com.skillstorm.project1.repositories.ItemRepository;
 
 @Service
 public class ItemService {
 
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
     public List<Item> getItems() {
-        return null;
+        return itemRepository.findAll();
     }
 }
