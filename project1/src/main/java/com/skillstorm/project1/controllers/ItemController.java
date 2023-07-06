dfc370f229fc93409955b74317a28506200583ac
RicardoRuv <75161937+RicardoRuv@users.noreply.github.com>
2023-07-06 16:45:12 -0600

    Added ItemService class
    Created ItemService , added components @Service and modified ItemController so it knows about ItemService

--- project1/src/main/java/com/skillstorm/project1/controllers/ItemController.java
+++ project1/src/main/java/com/skillstorm/project1/controllers/ItemController.java
@@ -2,17 +2,28 @@ package com.skillstorm.project1.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skillstorm.project1.models.Item;
+import com.skillstorm.project1.services.ItemService;
 
 @RestController
 @RequestMapping("/item")
 public class ItemController {
 
+    private final ItemService itemService;
+
+    @Autowired // constructor injection
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
     public List<Item> getItems() {
-        return null;
+        return itemService.getItems();
     }
 
 }
---
+++ project1/src/main/java/com/skillstorm/project1/services/ItemService.java
@@ -0,0 +1,15 @@
+package com.skillstorm.project1.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.skillstorm.project1.models.Item;
+
+@Service
+public class ItemService {
+
+    public List<Item> getItems() {
+        return null;
+    }
+}
